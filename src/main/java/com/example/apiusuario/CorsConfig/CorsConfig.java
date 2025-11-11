43227d7680b90f89c7b79020eae79f3005012f20
Manuel Ramos <manuelramos09544@gmail.com>
2025-11-10 21:32:50 -0500

    Archivo de permiso para URL front end

---
+++ src/main/java/com/example/apiusuario/CorsConfig/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.apiusuario.CorsConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://front-end-loggin.vercel.app",
+                                "https://front-end-menu-app-cyan.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
